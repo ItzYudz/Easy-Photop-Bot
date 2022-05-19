86f0dc8b7eb7903eb64a16d3b7ce2c43872eaa59
ItzYudz <95725567+ItzYudz@users.noreply.github.com>
2022-05-18 21:29:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+//remember, on replit, go the the shell and type "npm i photop-client@latest" for all of this to work
 const { Client } = require("photop-client")
-const client = new Client({ username: "bot name", password: "bot password"}, { logSocketMessages: false })
-
-client.onPost = (post)=>{
-   post.connect();
-	if (post.text.match(`h!help`)) {
-		post.chat(`working`)
-post.onChat = (chat)=>{
+const client = new Client({ username: "bot name", password: "bot password" }, { logSocketMessages: false })
+//visit replit.com/@YudzDoge for more things you can learn
+client.onPost = (post) => {
+  post.connect();
+  if (post.text.match(`bot!connect`)) {
+    post.chat(`working`)//this is the command for when you post
+    post.onChat = (chat) => {
+      if (chat.text.startsWith('bot!test')) {
+        post.chat(`test completed`)//this is the command for when you chat in the post that says bot!connect
+      }
+    }
   }
-	if (chat.text.startsWith('bot!test')) {
-		post.chat(`test completed`)
-   }
-  }
+}
+//you can change anything in here, such as bot!connect to blue!start or bot!test to red!help or anything you really want
